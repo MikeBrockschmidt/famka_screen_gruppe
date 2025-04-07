fe5dfbae7848b5f859e6f343c8a133eb8a6b9a04
Mike Brockschmidt <info@mike-brockschmidt.de>
2025-04-07 16:33:49 +0200

    Lorem ipsum

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class CustomScreen extends StatelessWidget {
             alignment: Alignment.centerLeft,
             padding: const EdgeInsets.only(left: 30),
             child: const Text(
-              'Gruppe',
+              'Grppe',
               style: TextStyle(
                 fontSize: 61,
                 fontWeight: FontWeight.w900,
@@ -160,8 +160,7 @@ class CustomScreen extends StatelessWidget {
                       child: Padding(
                         padding: const EdgeInsets.all(26),
                         child: Text(
-                          'Längerer Text hier, der den mittleren Bereich ausfüllt und vertikal scrollbar ist. ' *
-                              30,
+                          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. ',
                           style: const TextStyle(
                               color: Color.fromARGB(255, 0, 0, 0),
                               fontSize: 16),
